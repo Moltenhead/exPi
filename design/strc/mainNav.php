1e8d129ace1795bbee665555d99b20480743bb7d
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-04 11:50:10 +0100

    remplacement du HTML de la barre de navigation

--- design/strc/mainNav.php
+++ design/strc/mainNav.php
@@ -1,18 +1,28 @@
-<div class="content_wrapper">
-  <h1>
-    <strong id="name">Charlie</strong>
-    <strong id="surname">Gardai</strong>
-  </h1>
-
-  <ul class="horizontal_list">
-    <li>
-      <a href="#">A</a>
-    </li>
-    <li>
-      <a href="#">B</a>
-    </li>
-    <li>
-      <a href="#">C</a>
-    </li>
-  </ul>
-</div>
+<div id="header_wrapper">
+  <a class="home_link" href=""><strong>W/</strong>ole</a>
+  <div class="stretched_row">
+    <div id="main_nav" class="spaced_row">
+      <form class="search_bar spaced_row" action="" method="">
+        <input type="text" placeholder="Envie de ...">
+        <input type="submit" value="et je trouve !">
+      </form>
+      <nav class="link_box spaced_row">
+        <a href="">August</a>
+        <a href="">Bertholt</a>
+        <a href="">Camille</a>
+        <a href="">Diana</a>
+      </nav>
+    </div>
+    <div id="account_nav" class="connection_box spaced_row">
+      <button id="profile_options_button">
+        <svg>
+          <g>
+            <path points="0,0 30,0 15,30"></path>
+          </g>
+        </svg>
+      </button>
+      <a href="" id="profile">
+        <img class="micro_picto avatar" src="" alt="">
+      </a>
+    </div>
+  </div>
