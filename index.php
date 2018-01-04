3d0fc4c9481d9ea2d1916d5fa44bc0a9f10e9a69
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-04 10:25:15 +0100

    édition de l'url pour intégration globals.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <?php
-require_once(htmlspecialchars($_SERVER['DOCUMENT_ROOT']) . '/design/strc/globals.php');
+require_once(htmlspecialchars($_SERVER['DOCUMENT_ROOT']) . '/wole/design/strc/globals.php');
 require_once(objPath('strc', 'db_connexion.php'));
 ?>
 
