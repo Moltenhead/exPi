9aae9a29048dfc88f39a67b85f8c2cc0030761b2
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-06 01:56:38 +0100

    édition de la description

--- design/strc/head.php
+++ design/strc/head.php
@@ -3,7 +3,7 @@
 
 <title>Charlie GARDAI, web d&eacute;veloppeur et cr&eacute;ateur de contenus multim&eacute;dias</title>
 
-<meta name="description" content="Rencontrez Charlie GARDAI, jeune web d&eacute;veloppeur et concepteur multim&eacute;dia fran&ccedil;ais." />
+<meta name="description" content="Wole" />
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
