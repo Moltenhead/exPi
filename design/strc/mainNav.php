fb71bb8ac65b78a1b517401d94e5edbbd007103a
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-11 09:53:34 +0100

    édition sur la main_nav petits changements su la css

--- design/strc/mainNav.php
+++ design/strc/mainNav.php
@@ -1,27 +1,25 @@
-<div class="main_wrapper v_align">
+<div class="main_wrapper flex_row v_align">
   <a class="home_link" href="">
-    <strong>W/</strong>ole
+    e<strong>x&#1051;</strong>i
   </a>
-  <div class="flex_row stretched v_align">
-    <div id="global_nav" class="flex_row spaced">
-      <form class="search_bar flex_row spaced" action="" method="">
-        <input type="text" placeholder="Envie de ...">
-        <input type="submit" value="et je trouve !">
-      </form>
-      <nav class="link_box flex_row spaced">
-        <a href="">August</a>
-        <a href="">Bertholt</a>
-        <a href="">Camille</a>
-        <a href="">Diana</a>
-      </nav>
-    </div>
-    <div id="account_nav" class="connection_box flex_row spaced v_align">
-      <button id="profile_options_button" class="activable" title="options de compte">
-        <?php echo file_get_contents(objPath('img', 'svg/menu_arrow.svg')); ?>
-      </button>
-      <a href="" id="profile" title="accéder à votre profil">
-        <img class="micro_picto avatar" src="" alt="">
-      </a>
-    </div>
+  <div id="global_nav" class="flex_row spaced">
+    <form class="search_bar flex_row spaced" action="" method="">
+      <input type="text" placeholder="Envie de ...">
+      <input type="submit" value="et je trouve !">
+    </form>
+    <nav class="link_box flex_row spaced">
+      <a href="">August</a>
+      <a href="">Bertholt</a>
+      <a href="">Camille</a>
+      <a href="">Diana</a>
+    </nav>
+  </div>
+  <div id="account_nav" class="connection_box flex_row spaced v_align">
+    <button id="profile_options_button" class="activable" title="options de compte">
+      <?php echo file_get_contents(objPath('img', 'svg/menu_arrow.svg')); ?>
+    </button>
+    <a href="" id="profile" title="accéder à votre profil">
+      <img class="micro_picto avatar" src="" alt="">
+    </a>
   </div>
 </div>
