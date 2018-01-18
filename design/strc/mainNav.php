2e28cc2f082973d8d5d4c20398a92f8450ef474b
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-18 12:16:29 +0100

    modification du svg + édition css generale

--- design/strc/mainNav.php
+++ design/strc/mainNav.php
@@ -1,16 +1,17 @@
-<div class="main_wrapper flex_row v_align">
+<div class="main_wrapper flex_row aligned">
   <a class="home_link logo no_depth" href="" title="exPi">
       <?php echo file_get_contents(objPath('img', 'svg/exPi_logo_v8.svg')); ?>
   </a>
-  <div id="global_nav" class="flex_row spaced">
+  <div id="global_nav" class="flex_row spaced aligned">
     <form class="search_bar flex_row spaced" action="" method="">
       <input type="text" placeholder="Envie de ...">
       <input type="submit" value="et je trouve !">
     </form>
     <nav class="link_box flex_row spaced">
+      <a href="" class="micro_picto"><?php echo file_get_contents(objPath('img', 'svg/picto_eye.svg')) ?></a>
     </nav>
   </div>
-  <div id="account_nav" class="connection_box flex_row spaced v_align">
+  <div id="account_nav" class="connection_box flex_row spaced aligned">
     <?php if (session_status() !== PHP_SESSION_NONE) { ?>
     <button id="profile_options_button" class="activable" title="options de compte">
       <?php echo file_get_contents(objPath('img', 'svg/menu_arrow.svg')); ?>
