bfbf7fc9553234f58bbadc968a8ea68950026834
Charlie Gardai <charliegardai.pro@gmail.com>
2018-01-19 15:00:34 +0100

    midofs générale des css - rangement

--- design/strc/mainNav.php
+++ design/strc/mainNav.php
@@ -5,7 +5,6 @@
   <div id="global_nav" class="flex_row spaced aligned">
     <form class="search_bar flex_row spaced" action="" method="">
       <input type="text" name="search" placeholder="Envie de ...">
-      <input type="submit" value="et je trouve !">
       <select name="sens_filter">
         <option value="" class="micro_picto"><img src=<?php echo '"' . objPath('img', 'svg/picto_eye.svg') . '"'; ?>></option>
         <option value="" class="micro_picto"><?php echo file_get_contents(objPath('img', 'svg/picto_eye.svg')) ?></option>
@@ -13,6 +12,7 @@
         <option value="" class="micro_picto"><?php echo file_get_contents(objPath('img', 'svg/picto_eye.svg')) ?></option>
         <option value="" class="micro_picto"><?php echo file_get_contents(objPath('img', 'svg/picto_eye.svg')) ?></option>
       </select>
+      <input type="submit" value="et je trouve !">
     </form>
     <nav class="link_box flex_row spaced">
       <a href="" class="micro_picto" title="voir"><?php echo file_get_contents(objPath('img', 'svg/picto_eye.svg')) ?></a>
