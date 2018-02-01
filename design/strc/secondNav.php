6b8b318c3ca496b5176da035ba53846f6963e4e7
Charlie Gardai <charliegardai.pro@gmail.com>
2018-02-01 15:42:21 +0100

    initiating PHP structure for #secondNav

--- design/strc/secondNav.php
+++ design/strc/secondNav.php
@@ -1,46 +1,31 @@
-<div class="main_wrapper flex_column aligned">
-<?php if (isset($page)) { // TODO: links management through mySQL queries
-  if (in_array($page, $interest_menu)) { ?>
-  <h4 class="interest_header"></h4>
+<div class="main_wrapper flex_column centered aligned">
+<?php
+$req_second_nav = $bdd->prepare('SELECT p.title,
+                                        p.first_section_title AS 1st_section,
+                                        p.second_section_title AS 2nd_section,
+                                        nl.section_id,
+                                        nl.title,
+                                        nl.href
+                                  FROM pages AS p
+                                    INNER JOIN nav_links AS nl
+                                    ON nl.id_page = p.id
+                                      WHERE p.id = ?
+                                ');
+if (isset($page_id)) {
+  $req_second_nav->execute(array($page_id))?>
+  <h4 class="interest_header"><?php $second_nav_data[] ?></h4>
   <ul class="link_box">
-    <li>Auguste</li>
-    <li>Bertholt</li>
-    <li>Calvin</li>
-    <li>Dito</li>
-    <li>Fasma</li>
-    <li>Geralt</li>
+  <?php ?>
   </ul>
-  <h4 class="interest_header"></h4>
+  <?php if (/*hasDualMenu*/) { ?>
+  <h4 class="interest_header"><?php ?></h4>
   <ul class="link_box">
-    <li>Auguste</li>
-    <li>Bertholt</li>
-    <li>Calvin</li>
-    <li>Dito</li>
-    <li>Fasma</li>
-    <li>Geralt</li>
+  <?php ?>
   </ul>
-<?php } else if (in_array($page, $profile_menu)) { ?>
-  <h4 class="interest_header"></h4>
-  <ul class="link_box">
-    <li>Auguste</li>
-    <li>Bertholt</li>
-    <li>Calvin</li>
-    <li>Dito</li>
-    <li>Fasma</li>
-    <li>Geralt</li>
-  </ul>
-  <h4 class="interest_header"></h4>
-  <ul class="link_box">
-    <li>Auguste</li>
-    <li>Bertholt</li>
-    <li>Calvin</li>
-    <li>Dito</li>
-    <li>Fasma</li>
-    <li>Geralt</li>
-  </ul>
+  <?php } ?>
 <?php }
 } else { ?>
-  <h4 class="interest_header"></h4>
+  <h4 class="interest_header">Mes intérêts</h4>
   <ul class="link_box">
     <li>Auguste</li>
     <li>Bertholt</li>
@@ -49,7 +34,7 @@
     <li>Fasma</li>
     <li>Geralt</li>
   </ul>
-  <h4 class="interest_header"></h4>
+  <h4 class="interest_header">Intérêts collectifs</h4>
   <ul class="link_box">
     <li>Auguste</li>
     <li>Bertholt</li>
