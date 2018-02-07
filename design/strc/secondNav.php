55762a324f6808825595783732dace502d08ee1c
Charlie Gardai <charliegardai.pro@gmail.com>
2018-02-07 10:11:32 +0100

    arrow.svg integration within #secondNav and a little advancement within PHP structure

--- design/strc/secondNav.php
+++ design/strc/secondNav.php
@@ -2,24 +2,28 @@
 <?php //TODO: insert needed tables for the following code to work
 //section titles query
 //links query
-$que_nav_links = $db->prepare('SELECT section_id, title, class, href
+$que_nav_links = $db->prepare('SELECT COUNT(*) AS nb, section_id, title, class, href
                                   FROM nav_links
                                     WHERE page_id = ?
                               ');
 
 //checking if there is a page_id for the active page
 if (!empty($page_inf->get('id'))) {
-  $data_nav_links = $que_nav_links->fetchAll(PDO::FETCH_ASSOC);
-  if (count($data_nav_links) >= 1) {
-    $data_nav_links = $data_nav_links[0];
-  }
+  $que_nav_links->execute(array($page_inf->get('id')));
+  $data_nav_links = $que_nav_links->fetchAll(PDO::FETCH_COLUMN, 0);
+  $data_nav_links = $data_nav_links[0];
+  $que_nav_links->closeCursor();
 
+  $l_collections = array();
 ?>
   <h4 class="section_header"><?php echo $page_inf->get_section(0, 'title'); ?></h4>
   <ul class="link_box">
-  <?php// while () { ?>
+  <?php if ($nb_lines >= 1) {
+    while ($data_nav_links = $que_nav_links->fetch(PDO::FETCH_ASSOC);) { ?>
     <li><?php// $links[0]->print_link(i); i++; ?></li>
-  <?php// } ?>
+  <?php } else { ?>
+    <li>no links </li>
+  <?php } ?>
   </ul>
   <?php if ($page_inf->has_mutiple()) { ?>
   <h4 class="section_header"><?php echo $page_inf->get_section(1, 'title'); ?></h4>
@@ -31,6 +35,6 @@ if (!empty($page_inf->get('id'))) {
   <h4 class="section_header">No page index</h4>
 <?php } ?>
 </div>
-<button type="button">
-  <!-- TODO: ajouter le svg en forme de flèche -->
+<button class="picto micro_picto">
+  <?php echo file_get_contents(objPath('img', 'svg/arrow.svg')); ?>
 </button>
