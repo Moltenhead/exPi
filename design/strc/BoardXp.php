13a6c4078ab9f6a9ba795d95a01e02472a90430a
Charlie Gardai <charliegardai.pro@gmail.com>
2018-02-28 17:12:14 +0100

    styling: global xp blocks style

--- design/strc/BoardXp.php
+++ design/strc/BoardXp.php
@@ -4,32 +4,34 @@ class BoardXp extends Xp
   public function print()
   { ?>
     <a class="xp_wrapper">
-      <div class="xp flex_row">
-        <img src="<?php $this->_href ?>" alt="<?php echo $this->_alt ?>">
-        <div class="xp_informations">
-          <article class="slide_description">
-            <h6><?php echo $this->_title; ?></h6>
-            <?php echo $this->_short_description; ?>
-          </article>
-          <mark>
-            <strong>&Eacute;dité le </strong>
-            <span><?php echo $this->_date_update; ?></span>
-          </mark>
+      <div class="true_box">
+        <div class="xp flex_row">
+          <img src="<?php $this->_href ?>" alt="<?php echo $this->_alt ?>">
+          <div class="xp_informations">
+            <article class="slide_description">
+              <h6><?php echo $this->_title; ?></h6>
+              <?php echo $this->_short_description; ?>
+            </article>
+            <mark>
+              <strong>&Eacute;dité le </strong>
+              <span><?php echo $this->_date_update; ?></span>
+            </mark>
+          </div>
         </div>
-      </div>
-      <nav class="flex_column hovering_nav">
-      <?php if (isConnected()) {
-        if (in_array($interest101->get('xps'), $this->_id)) {?>
-        <a href="<?php echo objPath('mod', '') ?>" title="$Ccedil;a m'intéresse">
-          +
-        </a>
-        <?php } else { ?>
-        <a href="<?php echo objPath('mod', '') ?>" title="$Ccedil;a ne m'intéresse plus">
-          -
-        </a>
+        <nav class="flex_column hovering_nav">
+        <?php if (isConnected()) {
+          if (in_array($interest101->get('xps'), $this->_id)) {?>
+          <a href="<?php echo objPath('mod', '') ?>" title="$Ccedil;a m'intéresse">
+            +
+          </a>
+          <?php } else { ?>
+          <a href="<?php echo objPath('mod', '') ?>" title="$Ccedil;a ne m'intéresse plus">
+            -
+          </a>
+          <?php } ?>
         <?php } ?>
-      <?php } ?>
-      </nav>
+        </nav>
+      </div>
     </a>
   <?php }
   //TODO: replace "Ca m'intéresse" by a button with ajax db treatments on click
