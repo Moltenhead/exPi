da564f19a9e515d41ab3daaf64649686edb764ee
Charlie Gardai <charliegardai.pro@gmail.com>
2018-03-05 11:33:41 +0100

    add: Pagination object first iteration

--- lib/php/obj/Pagination.php
+++ lib/php/obj/Pagination.php
@@ -1,30 +1,47 @@
 <?php
 class Pagination
 {
-  private $_max_display = 5;//maximum displayed links to pages
+  private $_max_display = 5;//maximum displayed indexes
+  //declarations
+  private $_first_index;
+  private $_max_page;
+  private $_actual_page;
 
-  protected $_max_page;
-  protected $_actual_page;
-
-  private $pdo = $db;
-  private function __construct()
+  //ask for a nb of rows returned by a query on construct
+  private function __construct($nb_rows)
   {
     $this->_actual_page = (isset($_GET['page']) && $_GET['page'] != null) ?
       (int) $_GET['page'] :
-      1;
+      1;//if no get_page then page is the first
 
-    $data_nb_lines = $que_nb_lines->fetch(PDO::FETCH_COLUMN, 0);
-    $this->_max_page = $data_nb_lines / ($pagination + $slides_number);
+    $this->_max_page = $nb_rows / ($pagination + $slides_number);
+
+    $this->$_first_index = ($this->_actual_page <= round($this->_max_display / 2)) ?
+      1:
+      $this->_actual_page - round($this->_max_display / 2);
   }
 
   public function print()
   { ?>
-    <form action="?wh<?php echo htmlspecialchars($_GET['wh']); ?>" method="POST" class="pagination_wrapper">
+    <form action="?wh<?php echo htmlspecialchars($_GET['wh']); ?>"
+      method="POST"
+      class="pagination_wrapper"
+    >
       <input type="hidden" name="search" value="<?php echo $_POST['search']; ?>">
-      <?php for ($i = 0; $i < $this->_max_display; $i++) { ?>
-      <input type="submit" name="page" value="<?php echo $i; ?>">
+      <?php for (
+        $i = $this->_first_index;
+        $i < ($this->_first_index + $this->_max_display);
+        $i++
+      ) { ?>
+      <input type="submit" name="page" value="<?php
+        echo $i;
+        echo ($i === $this->_actual_page) ?
+          '" class="actual' :
+          '';
+      ?>
+      ">
       <?php } ?>
-    </nav>
+    </form>
   <?php }
 }
 ?>
