5d7e9e28e7148f5a6f43a3bfe2b934f656faf972
harmantes34 <harmantes@hotmail.fr>
2018-03-12 15:28:12 +0100

    add: nev_xp_creation view - wrong redirection

---
+++ lib/php/view/nav_xp_creation.php
@@ -0,0 +1,6 @@
+<nav class="nav_wrapper">
+  <form class="flex_row" action="index.html" method="get">
+    <input type="text" name="disp" value="create">
+    <input type="text" name="disp" value="how-to">
+  </form>
+</nav>
