224d8a9d1bbcc9e28128e15962bc42002e38fc1e
harmantes34 <harmantes@hotmail.fr>
2018-03-12 15:34:42 +0100

    add: navigation between displays

--- lib/php/view/nav_xp_creation.php
+++ lib/php/view/nav_xp_creation.php
@@ -1,6 +1,6 @@
 <nav class="nav_wrapper">
-  <form class="flex_row" action="index.html" method="get">
-    <input type="text" name="disp" value="create">
-    <input type="text" name="disp" value="how-to">
+  <form class="flex_row" action="<?php echo HTTPH; ?>?wh=creation_experience" method="post">
+    <input type="submit" class="disp_button" name="disp" value="create">
+    <input type="submit" class="disp_button" name="disp" value="how-to">
   </form>
 </nav>
