d21a0db0c2a9396d5c4ec7a28cd57215e85b048b
harmantes34 <harmantes@hotmail.fr>
2018-03-12 14:59:09 +0100

    add: new .hidden CSS class + initializing xp_creation view

--- lib/php/view/xp_create.php
+++ lib/php/view/xp_create.php
@@ -1,2 +1,19 @@
-<?php
-?>
+<div class="xp_wrapper">
+  <form
+    id="xp_datas"
+    class="hidden"
+    action="<?php echo objPath('mod', 'xp_create.php') ?>"
+    method="post">
+    </form>
+
+  <div class="">
+    <figure>
+      <figcaption></figcaption>
+      <img src="" alt="">
+    </figure>
+
+  </div>
+
+
+
+</div>
