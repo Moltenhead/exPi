abe30365646b038feafd125b089c60536690de74
Moltenhead <ectopneo@gmail.com>
2018-03-27 15:08:58 +0200

    edit: db connection for docker

--- design/strc/db_connexion.php
+++ design/strc/db_connexion.php
@@ -1,9 +1,9 @@
 <?php
-$db__host = 'localhost';
+$db__host = '0.0.0.0:9006';
 $db__name = 'expi_dev';
 
-$db__user = 'root';
-$db__pw = '';
+$db__user = 'expi_master';
+$db__pw = 'Pr0t3ctTh3M@st3r';
 
 try {
     $db = new PDO("mysql:host=$db__host;dbname=$db__name", $db__user, $db__pw);
