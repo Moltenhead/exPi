fc8a0dcede0dbe949a9dd81f4c4d44bcbe4074b2
Moltenhead <ectopneo@gmail.com>
2018-03-27 15:21:15 +0200

    add: environnement controller

--- design/strc/db_connexion.php
+++ design/strc/db_connexion.php
@@ -1,9 +1,15 @@
 <?php
-$db__host = '0.0.0.0:9006';
-$db__name = 'expi_dev';
-
-$db__user = 'expi_master';
-$db__pw = 'Pr0t3ctTh3M@st3r';
+if (isset($_ENV['MYSQL_HOST']) && $_ENV['MYSQL_HOST'] === 'db_expi') {
+  $db__host = '0.0.0.0:9006';
+  $db__user = 'expi_master';
+  $db__pw = 'Pr0t3ctTh3M@st3r';
+} else {
+  $db__host = 'localhost';
+  $db__user = 'root';
+  $db__pw = '';
+}
+$db__name = 'expi_dev';
+
 
 try {
     $db = new PDO("mysql:host=$db__host;dbname=$db__name", $db__user, $db__pw);
