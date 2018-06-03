a490638fc760e7ffda0936ac74e7b95d57a299c9
Moltenhead <ectopneo@gmail.com>
2018-06-03 18:33:08 +0200

    add: secondNav sections displaying depending on connected account presence

--- lib/php/view/secondNav_sections.php
+++ lib/php/view/secondNav_sections.php
@@ -1,7 +1,11 @@
 <?php
 for ($i = 0; $i < count($where_inf->nav_sections); $i++) {
-  if ((!isConnected() && ($i < 2 || $i > 3)) || isConnected()) {
-    $where_inf->showNav($i, '_self');
+  if (
+    (!isConnected() &&
+    ($where_inf->getNav($i,'id') != 2 && $where_inf->getNav($i,'id') != 3)) ||
+    isConnected()
+  ) {
+    $where_inf->showNav($i, '_self', isConnected());
   }
 }
 ?>
