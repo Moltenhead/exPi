c489de718a1e7e762d6343f4e04901867da55699
Moltenhead <ectopneo@gmail.com>
2018-06-03 18:43:57 +0200

    add: better and  beautifull key reassignement if posted values && smooth image management && type_class detection for formating

--- lib/php/view/xp_edition.php
+++ lib/php/view/xp_edition.php
@@ -1,4 +1,31 @@
-<div id="xp_edition_wrapper" class="xp_cu flex_column aligned slider_wrapper up_nav no_view_adjust">
+<?php
+require_once(objPath('mod', 'xp_get.php'));
+
+if (!empty($_POST['title'])) {
+  foreach ($_POST as $key => $value) {
+    if (array_key_exists($key, $xp)) {
+      $xp->key = $value;
+    }
+  }
+}
+
+$img = ROOT . "uploads/img/" . $xp->img;
+
+$img_path = (file_exists($img)) ?
+  @objPath('up_img', $xp->img) :
+  objPath('img', 'bitmap/exPi_logo_placeholder.png');
+?>
+
+<div
+  id="xp_edition_wrapper"
+  class=
+    "xp_cu
+    flex_column
+    aligned
+    slider_wrapper
+    up_nav
+    no_view_adjust
+    <?php echo $xp->type_class; ?>">
   <nav class="slider_nav flex_row stretched">
     <button class="slider_button go_left">&Eacute;diter</button>
     <button class="slider_button go_right">Charte</button>
